797dc7e8477b20f3bb10600f873379f139865e6e
lachiefish <lachiefish@gmail.com>
2017-07-05 16:58:05 +1000

    fixed file sending

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/privacy', function(req, res) {
-  res.sendFile(path.join(__dirname + '/privacy.html'));
+  res.sendFile(__dirname + '/privacy.html');
 });
 
 //Cannot deal with multiple post requests at one time
