10d090770e5b29647e009a571e285c160b5f598d
lachiefish <lachiefish@gmail.com>
2017-07-19 19:25:02 +1000

    Cluster removed

--- index.js
+++ index.js
@@ -1,181 +1,133 @@
-var cluster = require('cluster');
-if (cluster.isMaster) {
-  var express = require('express');
-  var app = express();
-  var bodyParser = require('body-parser');
+/*jshint esversion: 6 */
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var Horseman = require('node-horseman');
+var himalaya = require('himalaya');
+var util = require('util');
+var async = require('async');
+var queue = require('express-queue');
 
-  app.use(bodyParser.json()); // support json encoded bodies
-  app.use(bodyParser.urlencoded({
-    extended: true
-  })); // support encoded bodies
+var timetableURL = null;
+var date = null;
+var timetableHTML = null;
+var count = 8;
+var completeJSON = [];
+var day = null;
+var username = "";
+var password = "";
+var postResponse = null;
 
-  app.get('/', function(req, res) {
-    res.send('');
-  });
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // support encoded bodies
 
-  app.get('/privacy', function(req, res) {
-    res.sendFile(__dirname + '/privacy.html');
-  });
+app.get('/', function(req, res) {
+  res.send('');
+});
 
-  app.listen(process.env.PORT || 3000, function() {
-    console.log('Running Timetable Server');
-  });
+app.get('/privacy', function(req, res) {
+  res.sendFile(__dirname + '/privacy.html');
+});
 
-  app.post('/', function(req, res) {
-    var worker = cluster.fork();
-    var username = req.body.username;
-    var password = req.body.password;
-    worker.send({
-      "username": username,
-      "password": password,
-    });
-    worker.on('message', function(msg) {
-      if (msg !== null) {
-        res.send(msg);
-        res.end();
-      } else {
-        res.status(500);
-        res.end();
-      }
-      worker.on('exit', function() {
-        console.log('Worker exited.');
-      });
-    });
-  });
-}
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Running Timetable Server');
+});
 
-if (cluster.isWorker) {
-  console.log("This is worker " + process.pid);
-  require('newrelic');
-  var Horseman = require('node-horseman');
+app.post('/', function(req, res) {
   var horseman = new Horseman();
-  var himalaya = require('himalaya');
-  var util = require('util');
-  var async = require('async');
-  var queue = require('express-queue');
+  var username = req.body.username;
+  var password = req.body.password;
+  var responseText = new Promise((resolve, reject) => {
+      resPromise(resolve, reject);
+    })
+    .catch(err => error(err))
+    .then(responseText => {
+      res.send(responseText);
+    });
+});
 
-  var timetableURL = null;
-  var date = null;
-  var timetableHTML = null;
-  var count = 8;
-  var completeJSON = [];
-  var day = null;
-  var username = "";
-  var password = "";
-  var postResponse = null;
+function resPromise(resolve, reject) {
+  loadSpaces()
+    .catch(err => error(err))
+    .then(attribute => {
+      timetableURL = JSON.parse(attribute).uri;
+      var URLBeforeDate = timetableURL.search('=');
+      timetableURL = timetableURL.slice(0, URLBeforeDate + 1);
+      getTimetableData()
+        .catch(err => error(err))
+        .then(timetableHandler);
+    });
 
-  process.on('message', function(msg) {
-    var username = msg.username;
-    var password = msg.password;
-    loadSpaces()
+  function timetableHandler(data) {
+    dataToJSON(data);
+    count = count + 1;
+    getTimetableData()
       .catch(err => error(err))
-      .then(attribute => {
-        timetableURL = JSON.parse(attribute).uri;
-        var URLBeforeDate = timetableURL.search('=');
-        timetableURL = timetableURL.slice(0, URLBeforeDate + 1);
-        getTimetableData()
-          .catch(err => error(err))
-          .then(timetableHandler);
-      });
+      .then(timetableHandler);
+  }
 
-      function timetableHandler(data) {
-        dataToJSON(data);
-        count = count + 1;
-        getTimetableData()
-          .catch(err => error(err))
-          .then(timetableHandler);
-      }
+  function loadSpaces() {
+    completeJSON = [];
+    count = 8;
+    var timetableURLPromise =
+      horseman
+      .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
+      .cookies([])
+      .open('https://spaces.newington.nsw.edu.au/signin')
+      .click("body > div.container > div.row.button > div > section:nth-child(1) > a")
+      .waitForNextPage({
+        timeout: 15000
+      })
+      .type("#user_email", username)
+      .type("#user_password", password)
+      .click("#user_submit")
+      .waitForNextPage({
+        timeout: 15000
+      })
+      .url()
+      .waitForNextPage({
+        timeout: 15000
+      })
+      .waitForSelector('#diary_timetable_1', {
+        timeout: 10000
+      })
+      .attribute('#diary_timetable_1', "data-view-loadable-view");
+    return timetableURLPromise;
+  }
 
-    function loadSpaces() {
-      completeJSON = [];
-      count = 8;
-      var timetableURLPromise =
-        horseman
-        .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
-        .cookies([])
-        .open('https://spaces.newington.nsw.edu.au/signin')
-        .click("body > div.container > div.row.button > div > section:nth-child(1) > a")
-        .waitForNextPage({
-          timeout: 15000
-        })
-        .type("#user_email", username)
-        .type("#user_password", password)
-        .click("#user_submit")
-        .waitForNextPage({
-          timeout: 15000
-        })
-        .url()
-        .waitForNextPage({
-          timeout: 15000
-        })
-        .waitForSelector('#diary_timetable_1', {
-          timeout: 10000
-        })
-        .attribute('#diary_timetable_1', "data-view-loadable-view");
-      return timetableURLPromise;
-      //  .on("error", function() {
-      //   horseman.catch(function(err) {
-      //     error(err);
-      //   });
-      // })
+  function getTimetableData() {
+    if (count <= 19) {
+      if (count < 10) {
+        date = "2017-05-0" + count;
+      } else if (count == 13 || count == 14) {
 
-      // timetableURLPromise.then(function(attribute) {
-      //   try {
-      //     console.log("URL = " + attribute);
-      //     timetableURL = attribute;
-      //     var URLBeforeDate = timetableURL.search('=');
-      //     timetableURL = timetableURL.slice(8, URLBeforeDate + 1);
-      //     getTimetableData();
-      //   } catch (err) {
-      //     error(err);
-      //   }
-      // });
-    }
-
-    function getTimetableData() {
-      if (count <= 19) {
-        if (count < 10) {
-          date = "2017-05-0" + count;
-        } else if (count == 13 || count == 14) {
-
-        } else {
-          date = "2017-05-" + count;
-        }
-        var timetableHTMLPromise =
-          horseman
-          .open("https://spaces.newington.nsw.edu.au" + timetableURL + date)
-          .waitForSelector("body > table > tbody")
-          .html("body > table > tbody");
-        return timetableHTMLPromise;
-        // timetableHTMLPromise.then(function(data) {
-        //   try {
-        //     dataToJSON(data);
-        //     count = count + 1;
-        //     getTimetableData();
-        //   } catch (err) {
-        //     error(err);
-        //   }
-        // });
       } else {
-        console.log("DONE");
-        var JSONstring = JSON.stringify(completeJSON);
-        JSONstring = JSONstring.replace(/&amp;/g, "&");
-        process.send(JSONstring);
-        process.exit(1);
+        date = "2017-05-" + count;
       }
+      var timetableHTMLPromise =
+        horseman
+        .open("https://spaces.newington.nsw.edu.au" + timetableURL + date)
+        .waitForSelector("body > table > tbody")
+        .html("body > table > tbody");
+      return timetableHTMLPromise;
+    } else {
+      console.log("DONE");
+      var JSONstring = JSON.stringify(completeJSON);
+      JSONstring = JSONstring.replace(/&amp;/g, "&");
+      resolve(JSONstring);
     }
-  });
+  }
 
   function dataToJSON(html) {
-    if (html !== null){
+    if (html !== null) {
       var json = himalaya.parse(html);
       organiseJSON(json);
-      console.log(err.message);
-      process.exit(1);
-  }else{
-    error(null);
+    } else {
+      error(null);
+    }
   }
-}
 
   function organiseJSON(json) {
     switch (count) {
@@ -257,10 +209,9 @@ if (cluster.isWorker) {
     };
     completeJSON.push(dayJSON);
   }
+}
 
-  function error(err) {
-    process.send(null);
-    console.log(err);
-    process.exit(1);
-  }
+function error(err) {
+  reject("There was an error");
+  console.log("Error: " + err);
 }
